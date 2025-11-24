d3a70530d0485267fa5ffdb2adfc7dedaf9c20b1
Prpkng <guiejo2020@gmail.com>
2025-11-23 22:55:25 -0300

    chore: base enemy class and structure

---
+++ Entities/World/Enemies/Common/BaseEnemy.gd
@@ -0,0 +1,7 @@
+class_name BaseEnemy
+extends CharacterBody2D
+
+## The base class of all enemies, with common functionality
+
+@onready var health: Health = $Health
+@onready var sprite: Sprite2D = $Sprite2D
